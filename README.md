eaf98564a4d2308472f321ef6d6fa1b7272837c4
Ning Shang <nshang@qti.qualcomm.com>
2015-03-23 22:39:46 -0700

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # twitbot-tih
 A Twitter bot that tweets today-in-history events
+
+# Dependencies
+
+- Python 2.7, and modules: wikipedia, twython.
+
+- Unix/Linux environment and the 'date' command
+
+# Usage
+
+1. Create a Twitter app and obtain the API Key, API Secret, Access
+Token, and Access Token Secret for the app. This can be done by
+following the instructions at:
+http://www.instructables.com/id/Raspberry-Pi-Twitterbot/?ALLSTEPS.
+
+2. Override the corresponding strings in the file '.auth' with
+appropriate Twitter app API access token strings obtained in the last
+step.
+
+3. Run 'python today_in_history_bot.py' to tweet.
+
+4. (Optional) Create a cron job to invoke the bot once a day.
+ 
---
+++ today_in_history_bot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sys
+import random
+import subprocess
+import wikipedia
+from twython import Twython
+
+
+ApiKey = ''
+ApiSecret = ''
+AccessToken = ''
+AccessTokenSecret = ''
+cred_file = './.auth'
+twitter_allowed_char = 140
+
+def get_api_token():
+    ''' Obtain Twitter app's API token from file .auth
+
+    Returns list
+    '''
+    f = open(cred_file, 'rb')
+    c = f.read()
+    t = c.splitlines()
+    return t[0:4]
+
+def get_today_str():
+    ''' Obtain current date in 'Month_Date' format, e.g., March 3
+
+    Returns str
+    '''
+    d = subprocess.check_output(["date", "+%B_%d"])
+    d_str = d.translate(None, '\n')
+    return d_str
+
+def get_events_list(date_str):
+    ''' Obtain the list of events for date_str
+
+    Returns list
+    '''
+    page = wikipedia.WikipediaPage(title=date_str)
+    events = page.section("Events")
+    events_list = events.splitlines()
+    return events_list
+
+def get_tweet_str():
+    ''' Obtain the string to tweet. It does sanity checks.
+
+    Returns str
+    '''
+    trials = 10
+    tweet_str = 'Nothing to tweet today.'
+    today_str = get_today_str()
+    events_list = get_events_list(today_str)
+    list_size = len(events_list)
+    if list_size == 0:
+        return tweet_str
+    
+    while trials > 0:
+        i = random.randrange(0, list_size)
+        entry_str = events_list[i]
+        if len(entry_str) + len(' #' + today_str) <= twitter_allowed_char:
+            tweet_str = entry_str + ' #' + today_str
+        else:
+            trials = trials - 1
+    
+    return tweet_str
+
+
+def do_tweet(str):
+    ''' Tweet str to Twitter
+
+    '''
+    [ApiKey, ApiSecret, AccessToken, AccessTokenSecret] = get_api_token()
+    api = Twython(ApiKey, ApiSecret, AccessToken, AccessTokenSecret)
+    api.update_status(status=str)
+    print "Tweeted: " + str
+
+
+if __name__ == '__main__':
+    tweet_str = get_tweet_str()
+    do_tweet(tweet_str)
+
+
+    
+
