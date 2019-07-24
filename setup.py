7ab614893ce183769d56300c8e933975b4af32c0
Ning Shang <syncom.dev@gmail.com>
2019-07-24 13:25:11 -0700

    Updated wikipedia with correct version Discovered the bug when trying 'python3 setup.py install' The bug is now fixed.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="1.0.0",
     description="A Twitter bot that tweets today-in-history events",
     install_requires=[
-        "wikipedia>=39.0.1",
+        "wikipedia>=1.4.0",
         "twython>=3.7.0",
         "pyOpenSSL>=19.0.0",
         "ndg-httpsclient>=0.5.1",
