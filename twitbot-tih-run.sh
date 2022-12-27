839aab4100a0e2c625b69cbb94c90c5ce505bcc3
Ning Shang <syncom.dev@gmail.com>
2022-12-27 10:59:28 -0800

    Fix shellcheck warnings (#2)
    Make shell script shellcheck clean.

--- twitbot-tih-run.sh
+++ twitbot-tih-run.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-ROOTDIR=`dirname $0`
-. ${ROOTDIR}/venv
-python ${ROOTDIR}/today_in_history_bot.py 
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly SCRIPT_DIR
+# shellcheck source=/dev/null
+. "${SCRIPT_DIR}/venv"
+python "${SCRIPT_DIR}/today_in_history_bot.py"
